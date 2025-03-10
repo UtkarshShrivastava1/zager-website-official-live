d3cf7abef2e26b1af3b8a475a59915e6a8ad7aea
UTKARSH SHRIVASTAVA <46999497+UtkarshShrivastava1@users.noreply.github.com>
2025-03-10 17:03:25 +0530

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        domains: ['firebasestorage.googleapis.com'], // Allow Firebase Storage URLs
-      },
-};
-
-export default nextConfig;
